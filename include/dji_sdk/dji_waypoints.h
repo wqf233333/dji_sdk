9aec86824775c7b67213b3f1e0ad451b869d3208
xuhao <xuhao3e8@gmail.com>
2015-06-09 16:25:17 +0800

    waypoints support

---
+++ include/dji_sdk/dji_waypoints.h
@@ -0,0 +1,15 @@
+//
+// Created by Hao Xu on 15/6/8.
+//
+
+#ifndef DJI_SDK_DJI_WAYPOINTS_H
+#define DJI_SDK_DJI_WAYPOINTS_H
+
+
+class dji_waypoints
+{
+
+};
+
+
+#endif //DJI_SDK_DJI_WAYPOINTS_H
---
+++ src/modules/dji_waypoints.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hao Xu on 15/6/8.
+//
+
+#include "dji_waypoints.h"
